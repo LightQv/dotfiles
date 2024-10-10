144fd32beb5fc728da47d9bf798cd6be8d4c5967
LightQv <QVivian23@gmail.com>
2024-10-10 10:18:43 +0200

    Fix zshenv

--- install.sh
+++ install.sh
@@ -8,18 +8,6 @@ SCRIPT_DIR="$PWD"
 . "$SCRIPT_DIR/scripts/lazydocker.sh"
 . "$SCRIPT_DIR/scripts/terminal.sh"
 
-ZSHENV="$HOME/.zshenv"
-
-add_to_zshenv() {
-    if ! grep -q "$1" "$ZSHENV"; then
-        echo "$1" >> "$ZSHENV"
-    fi
-}
-
-add_to_zshenv 'eval "$(/opt/homebrew/bin/brew shellenv)"'
-add_to_zshenv 'export PATH="/opt/homebrew/bin:$PATH"'
-add_to_zshenv 'export PATH="/usr/local/bin:$PATH"'
-
 . "$HOME/.config/zsh/.zshrc"
 
 cat << STONK
