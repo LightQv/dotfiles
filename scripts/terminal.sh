7d13eecede3a49a71705aa6387e1c858773aa2dd
LightQv <QVivian23@gmail.com>
2024-10-18 15:59:51 +0200

    Fix terminal script

--- scripts/terminal.sh
+++ scripts/terminal.sh
@@ -9,19 +9,19 @@ mkdir -p "$FONT_DIR"
 
 info "Downloading JetBrainsMono Nerd Font..."
 curl -# -L -o "$HOME/JetBrainsMono.zip" "$NERD_FONT_URL"
-if [ $? -eq 0 ]; then 
-	info "Unzipping the font files..." 
-	unzip -oq "$HOME/JetBrainsMono.zip" -d "$FONT_DIR" 
-	if [ $? -eq 0 ]; then 
+if [ $? -eq 0 ]; then
+	info "Unzipping the font files..."
+	unzip -oq "$HOME/JetBrainsMono.zip" -d "$FONT_DIR"
+	if [ $? -eq 0 ]; then
 		success "JetBrainsMono Nerd Font has been installed successfully!"
 		rm "$HOME/JetBrainsMono.zip"
-	else 
-		error "Failed to unzip JetBrainsMono Nerd Font." 
-		return 1 
-	fi 
-else 
-	error "Failed to download JetBrainsMono Nerd Font." 
-	return 1 
+	else
+		error "Failed to unzip JetBrainsMono Nerd Font."
+		return 1
+	fi
+else
+	error "Failed to download JetBrainsMono Nerd Font."
+	return 1
 fi
 
 ## Starship
@@ -34,14 +34,14 @@ fi
 info "Installing Starship..."
 brew install starship
 if [[ $? -eq 0 ]]; then
-	success "Starship has been installed successfully!" 
+	success "Starship has been installed successfully!"
 else
 	error "Kitty installation failed."
 fi
 
 STARSHIP_TOML_URL="https://raw.githubusercontent.com/lightqv/dotfiles/main/starship/starship.toml"
 mkdir -p "$HOME/.config/starship"
-info "Downloading starship.toml..." 
+info "Downloading starship.toml..."
 curl -# -o "$HOME/.config/starship/starship.toml" "$STARSHIP_TOML_URL"
 if [[ $? -eq 0 ]]; then
     success "starship.toml has been successfully downloaded."
@@ -52,17 +52,17 @@ fi
 ## Kitty
 info "Installing Kitty..."
 curl -# -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-if [ $? -eq 0 ]; then 
-	success "Kitty has been installed successfully!" 
-else 
+if [ $? -eq 0 ]; then
+	success "Kitty has been installed successfully!"
+else
 	error "Kitty installation failed."
-	return 1 
+	return 1
 fi
 
 mkdir -p "$HOME/.config/kitty"
 
 KITTY_CONF_URL="https://raw.githubusercontent.com/lightqv/dotfiles/main/kitty/kitty.conf"
-info "Downloading kitty.conf..." 
+info "Downloading kitty.conf..."
 curl -# -o "$HOME/.config/kitty/kitty.conf" "$KITTY_CONF_URL"
 if [[ $? -eq 0 ]]; then
     success "kitty.conf has been successfully downloaded."
@@ -73,7 +73,7 @@ fi
 mkdir -p "$HOME/.config/kitty/themes"
 
 MOCHA_KITTY_THEME_URL="https://raw.githubusercontent.com/lightqv/dotfiles/main/kitty/themes/mocha.conf"
-info "Downloading mocha.conf..." 
+info "Downloading mocha.conf..."
 curl -# -o "$HOME/.config/kitty/themes/mocha.conf" "$MOCHA_KITTY_THEME_URL"
 if [[ $? -eq 0 ]]; then
     success "mocha.conf has been successfully downloaded."
@@ -82,8 +82,8 @@ else
 fi
 
 CYBERPUNK_KITTY_THEME_URL="https://raw.githubusercontent.com/lightqv/dotfiles/main/kitty/themes/cyberpunk.conf"
-info "Downloading cyberpunk.conf..." 
-curl -# -o "$HOME/.config/kitty/themes/mocha.conf" "$CYBERPUNK_KITTY_THEME_URL"
+info "Downloading cyberpunk.conf..."
+curl -# -o "$HOME/.config/kitty/themes/cyberpunk.conf" "$CYBERPUNK_KITTY_THEME_URL"
 if [[ $? -eq 0 ]]; then
     success "cyberpunk.conf has been successfully downloaded."
 else
