6a7e35c11265f489dff330510c948d14b1d7fed6
vquerenet <v.querenet@leviatan.fr>
2025-08-01 08:59:57 +0200

    [DOT-03]: replace kitty by ghostty

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -11,10 +11,12 @@ alias ll='ls -la'                    # Detailed list with hidden files
 alias ls='ls --color=auto'           # Colorized ls (if supported)
 
 # Git
-alias gs='git status'                # Git status shortcut 
+alias gs='git status'                # Git status shortcut
 alias gcommit='git commit -m'            # Git commit with message
-alias gpush='git push origin $(git branch --show-current)' # Git push origin current branch
 alias gpod='git pull origin develop' # Git pull origin develop
+alias gpush='git push origin $(git branch --show-current)' # Git push origin current branch
+alias gswitch='git switch'                # Git switch shortcut
+alias gcb='git checkout -b'          # Git checkout on new branch
 
 # ZSH
 alias vzrc='vim "$HOME/.config/zsh/.zshrc"' # Open .zshrc with Vim
@@ -23,33 +25,12 @@ alias szrc='. "$HOME/.config/zsh/.zshrc"'  # Source .zshrc
 # Starship
 alias vship='vim "$HOME/.config/starship/starship.toml"' # Open starship.toml with Vim
 
-# Kitty's theme
-alias kitty_mocha='
-    sed -i "" "17s|.*|palette = \"catppuccin_mocha\"|" "$HOME/.config/starship/starship.toml" &&
-    
-    sed -i "" "2s|.*|#include $HOME/.config/kitty/themes/cyberpunk.conf|" "$HOME/.config/kitty/kitty.conf" &&
-    sed -i "" "3s|.*|include $HOME/.config/kitty/themes/mocha.conf|" "$HOME/.config/kitty/kitty.conf" &&
-
-    sed -i "" "8s|.*|[ -f \"$HOME/.config/zsh/themes/mocha-syntax-highlighting.zsh\" ] && source \"$HOME/.config/zsh/themes/mocha-syntax-highlighting.zsh\"|" "$HOME/.config/zsh/.zshrc" &&
-    sed -i "" "9s|.*|# [ -f \"$HOME/.config/zsh/themes/cyberpunk-syntax-highlighting.zsh\" ] && source \"$HOME/.config/zsh/themes/cyberpunk-syntax-highlighting.zsh\"|" "$HOME/.config/zsh/.zshrc" &&
-
-    szrc &&
-    kitty @ load-config
-'
-
-alias kitty_cyberpunk='
-    sed -i "" "17s|.*|palette = \"cyberpunk\"|" "$HOME/.config/starship/starship.toml" &&
-    
-    sed -i "" "2s|.*|#include $HOME/.config/kitty/themes/mocha.conf|" "$HOME/.config/kitty/kitty.conf" &&
-    sed -i "" "3s|.*|include $HOME/.config/kitty/themes/cyberpunk.conf|" "$HOME/.config/kitty/kitty.conf" &&
-
-    sed -i "" "8s|.*|# [ -f \"$HOME/.config/zsh/themes/mocha-syntax-highlighting.zsh\" ] && source \"$HOME/.config/zsh/themes/mocha-syntax-highlighting.zsh\"|" "$HOME/.config/zsh/.zshrc" &&
-    sed -i "" "9s|.*|[ -f \"$HOME/.config/zsh/themes/cyberpunk-syntax-highlighting.zsh\" ] && source \"$HOME/.config/zsh/themes/cyberpunk-syntax-highlighting.zsh\"|" "$HOME/.config/zsh/.zshrc" &&
-
-    szrc &&
-    kitty @ load-config
-'
+# Python
+alias python='python3.12'          # For ZSH
+alias python3='python3.12'         # Force Python3.12
 
 # Docker
-alias dc='docker-compose'          # Docker-compose
-alias dc='docker compose'          # Docker compose
+alias dc='docker-compose'          # Docker-compose shortcut
+
+# Lazydocker
+alias lzd='lazydocker'             # Lazydocker shortcut
